4b79995c944059ba22ceb63208e2b306a29e3ad1
Rodrigo Carlachiani <rodrigo@genosha.com.ar>
2023-04-17 12:57:00 -0300

    added date in real time

--- script.js
+++ script.js
@@ -1,4 +1,35 @@
 $(function () {
+
+    function getDateTime() {
+        var now = new Date(); 
+        var year = now.getFullYear();
+        var month = now.getMonth()+1; 
+        var day = now.getDate();
+        var hour = now.getHours();
+        var minute = now.getMinutes();
+        var second = now.getSeconds(); 
+
+        if (month.toString().length == 1) {
+            month = '0' + month;
+        } else if (day.toString().length == 1) {
+            day = '0' + day;
+        } else if (hour.toString().length == 1) {
+            hour = '0' + hour;
+        } else if (minute.toString().length == 1) {
+            minute = '0' + minute;
+        } else if (second.toString().length == 1) {
+            second = '0' + second;
+        }   
+        
+        let dateTime = day + '/' + month + '/' + year + ', ' + hour + ':' + minute + ':' + second;   
+        return dateTime;
+    }
+
+    setInterval(function() {
+        currentTime = getDateTime();
+        $('.date').text(currentTime)
+    }, 1000);
+
     let entry;
     let firstValue;
     let secondValue;
